df00b55485a1d883e2fe570d04ecb9a49259f70d
mrrsff <esbaltaci123@gmail.com>
2022-12-28 12:53:50 +0300

    added linux compatibility

--- tester.py
+++ tester.py
@@ -1,17 +1,17 @@
 import os
+from pathlib import Path
 import subprocess
 import filecmp
 
 def get_objs():
-    path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"objects")
-    files = os.listdir(path)
-    objs = [i for i in files if i.endswith('.obj')]
+    files = Path("./objects").glob('**/*')
+    objs = [i for i in files if i.suffix == '.obj']
     return objs
 
 print("Checking for your code...")
 needed_files = ["BinaryHeap.cpp","MeshGraph.cpp"]
 for i in needed_files:
-    if not os.path.exists(f".\\your_code\\{i}"):
+    if not Path(f"./your_code/{i}").exists():
         print(f"Missing {i}")
         exit()
 
@@ -20,20 +20,20 @@ os.system("g++ -o test.exe main.cpp")
 objs = get_objs()
 
 print("Cleaning up...")
-while(os.listdir(".\\output")):
-    os.remove(os.path.join(".\\output",os.listdir(".\\output")[0]))
+while(Path(".\\output").exists() and len(os.listdir(".\\output"))>0):
+    os.remove(Path(".\\output").glob("*")[0])
 
 print("Testing begins...")    
 passed = 0
 for i in range(len(objs)):
-    if(os.path.exists(f".\\output\\object{i}.txt")):
+    if(Path(f".\\output\\object{i}.txt").exists()):
         os.remove(f".\\output\\object{i}.txt")
     subprocess.run(f"test.exe >> .\\output\\object{i}.txt",shell=True, input=bytes(str(i),encoding='utf-8'))
     
     truth = filecmp.cmp(f".\\answers\\answer{i}.txt",f".\\output\\object{i}.txt")
     if truth:
         print(f"Object {i+1}/{len(objs)} passed")
-        os.remove(f".\\output\\object{i}.txt")
+        os.remove(Path(f"./output/object{i}.txt"))
         passed += 1
     else:
         print(f"Object {i+1}/{len(objs)} failed")
